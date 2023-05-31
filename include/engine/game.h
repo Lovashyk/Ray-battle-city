27d0aad449eb2c4fc5e979a84f2e8e7ed53866b8
Lovashyk <alexkosar376@gmail.com>
2023-06-01 00:16:13 +0300

    Moved engine related files to engine/ directory

---
+++ include/engine/game.h
@@ -0,0 +1,20 @@
+#include "raylib.h"
+#include <string.h>
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+
+
+class GameInit {
+    private:
+        std::string title;
+        int screenHeight;
+        int screenWidth;
+    public:
+       static void InitGame(int screenHeight, int screenWidth, std::string title);
+       std::string setTitle(std::string newTitle);
+       int setScreenHeight(int newHeight);
+       int setScreenWidth(int newWidth);
+
+};
+
